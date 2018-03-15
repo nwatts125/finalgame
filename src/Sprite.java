c346af56ab5fef04030cd642228b459e8039067e
tanksbuddy <tanksbuddy@users.noreply.github.com>
2018-03-15 14:19:16 -0600

    sprite

---
+++ src/Sprite.java
@@ -0,0 +1,34 @@
+
+
+public abstract class Sprite extends EzImage {
+    protected int dx, dy;
+    
+    public Sprite(int deltax, int deltay, String fpath){
+        super();
+        this.changeImage(fpath);
+        dx = deltax;
+        dy = deltay;
+    }
+    
+    public void setX(int x){
+        dx = x;
+    }
+
+    public void setY(int y){
+        dy = y;
+    }
+    
+    public void changeImage(String path){
+        this.setImage(path);
+    }
+    
+    public boolean collides(){
+        
+    }
+    
+    public abstract void move();
+    
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
