f80ea0651a2914a69037cac932782484652a2fb2
PhoenixRoseFromAshes <23475@student.murrayschools.org>
2018-03-15 14:18:45 -0600

    My stuff

---
+++ src/Sprite.java
@@ -0,0 +1,42 @@
+public abstract class Sprite extends EzImage{
+    protected int dx;
+    protected int dy;
+    protected String filepath;
+    public Sprite(int x, int y, int w, int h, int dex, int dey, String fp){
+        super(x,y,w,h);
+        dx = dex;
+        dy = dey;
+        filepath = fp;
+    }
+    public Sprite(){
+        super();
+        dx = 0;
+        dy = 0;
+        filepath = "";
+    }
+    public void setDX(int d){
+        dx = d;
+    }
+    public void setDY(int d){
+        dy = d;
+    }
+    public void imageChange(){
+        this.setImage(filepath);
+    }
+    public int getDX(){
+        return dx;
+    }
+    public int getDY(){
+        return dy;
+    }
+    // public int getX(){
+        // return x;
+    // }
+    // public int getY(){
+        // return y;
+    // }
+    public boolean collides(){
+        return false;
+    }
+    public abstract void move();
+}
