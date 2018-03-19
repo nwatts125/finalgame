f318b9ba0db04753c3203d22716129eadc1c0142
Kemp <jdkemp1@gmail.com>
2018-03-19 14:02:32 -0600

    the basic JFrame for where the games take place

--- Gameboi/UI.java
+++ Gameboi/UI.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
-public class UI
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class UI 
 {
     public JFrame win;
     public JButton close;
@@ -11,20 +13,25 @@ public class UI
         win.setLayout(null);
         win.setBounds(0,0,1000,700);
         win.setResizable(false);
+
         JButton close;
         close = new JButton("X");
         close.setBounds(0,0,25,25);
         close.setFont(new Font("Comic Sans", Font.BOLD, 30));
-        win.add(close, 0);
-        score = new JLabel("SCOREBOARD");
+        close.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    //add what is closed
+                }
+            });
+            win.add(close, 0);
         
-        win.setVisible(true);
+        score = new JLabel("OVERALL HIGH SCORE");
+        score.setBounds(0,5,150,50);
+        score.setFont(new Font("Comic Sans", Font.BOLD, 12));
+        win.add(score,0);
+        win.setVisible(true); 
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        
-    }
-    
-    }
 
-    
+    }
+}
 
