e9847fcb036d86bb4286e8bef255cba2bb6cce06
CarterWilde <carterjwilde@gmail.com>
2018-04-02 13:55:07 -0600

    Added some new docs to explain the classes
    Just to get some explaination to it

--- Models/FileManagement/FileEncoder.java
+++ Models/FileManagement/FileEncoder.java
@@ -7,6 +7,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 class FileEncoder{
+  /**
+   * Save takes in a object the implements Model.
+   * It will then save it to a .xml file and later be decoded.
+   */
   public void save(Model mdl){
     try{
         FileOutputStream fos = new FileOutputStream("saveFile.xml");
--- Models/FileManagement/Model.java
+++ Models/FileManagement/Model.java
@@ -1,4 +1,12 @@
 package Models.FileManagement;
+/**
+ * This is used to declare the data you want to be saved to a XML file.
+ * Use the method save from the class FileEncoder to save.
+ * You should make a new object that is used to save the data.
+ * for example if you want to save data about a person you should 
+ * make a new file called Person that has the data about the person
+ * like name age so on.
+ */
 
 interface Model{
   public void saveStrings(String[] s);
@@ -6,7 +14,7 @@ interface Model{
   public void saveBool(boolean[] b);
   public void saveList(Model[] m);
   public String[] getStrings();
-  public int[] saveInts();
-  public boolean[] saveBool();
-  public Model[] saveList();
+  public int[] getInts();
+  public boolean[] getBool();
+  public Model[] getList();
 }
