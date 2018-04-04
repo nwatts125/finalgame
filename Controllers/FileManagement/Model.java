512bae579fde9f83d45c7145c3ac621b7c9e43f2
Carter Wilde <carterjwilde@gmail.com>
2018-04-04 14:02:55 -0600

    Code Clean up and fixes

--- Controllers/FileManagement/Model.java
+++ Controllers/FileManagement/Model.java
@@ -3,16 +3,16 @@ package Models.FileManagement;
  * This is used to declare the data you want to be saved to a XML file.
  * Use the method save from the class FileEncoder to save.
  * You should make a new object that is used to save the data.
- * for example if you want to save data about a person you should 
+ * for example if you want to save data about a person you should
  * make a new file called Person that has the data about the person
  * like name age so on.
  */
 
 interface Model{
-  public void saveStrings(String[] s);
-  public void saveInts(int[] i);
-  public void saveBool(boolean[] b);
-  public void saveList(Model[] m);
+  public void setStrings(String[] s);
+  public void setInts(int[] i);
+  public void setBool(boolean[] b);
+  public void setList(Model[] m);
   public String[] getStrings();
   public int[] getInts();
   public boolean[] getBool();
