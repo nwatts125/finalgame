99fece80c90825720ce6ec795212b27eb697a08c
CarterWilde <carterjwilde@gmail.com>
2018-05-18 14:29:15 -0600

    UI Constructor
    Overloaded the constructor of the UI to include just a Boolean value - whether or not to include a scoreboard.

--- src/UI.java
+++ src/UI.java
@@ -5,6 +5,29 @@ class UI extends JFrame{
   JButton close;
   JComponent game;
   Scoreboard scoreBoard;
+  
+  public UI(boolean sb){
+    setBounds(0,0,1000,700);
+    setVisible(true);
+    setLayout(null);
+    setResizable(false);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //Scoreboard
+    if(sb){
+      scoreBoard = new Scoreboard(this, sb);
+      add(scoreBoard);
+    }
+    //Close Button
+    close = new JButton("X");
+    close.setBounds(0,0,45,45);
+    close.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        setVisible(false);
+      }
+    });
+    add(close,0);
+  }
+  
   public UI(int x, int y, int w, int h, boolean sb){
     setBounds(x,y,w,h);
     setVisible(true);
