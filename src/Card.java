232f296784cc9cd0aa1fa118ba680e96862ddd88
CarterWilde <carterjwilde@gmail.com>
2018-05-18 14:15:38 -0600

    Changed the code in the Card class

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.*;
@@ -10,123 +9,98 @@ public class Card extends JComponent implements MouseListener{
     protected String question;
     protected JLabel gWin;
     protected ImagePanel back;
-    protected JTextArea Inquiry;
+    protected ImagePanel Shape;
     public Card(int x, int y, int w, int h, String q){
         this.setBounds(x,y,w,h);
         question = q;
         matched=false;
 
-        back = new ImagePanel(x,y,w,h,"back.png");
+        back = new ImagePanel(0,0,w,h,"back.png");
+        back.setVisible(true);
         this.add(back);
 
-        Inquiry = new JTextArea(q) ;
-        Inquiry.setVisible(false);
-        Inquiry.setLineWrap(true);
-        Inquiry.setBounds(x,y,w,h);
-        this.add(Inquiry,0);
+        Shape = new ImagePanel(0,0,w,h,q) ;
+        Shape.setVisible(false);
+        Shape.setBounds(x,y,w,h);
+        this.add(Shape,0);
 
         addMouseListener(this);
     }
+    public Card(int x, int y, int w, int h, String q, String pic, int s){
+        this.setBounds(x,y,w,h);
+        question = q;
+        matched=false;
 
-    /**
-     * Returns Value that can be used to compare question & answer combos
-     *
-     * @return
-     */
+        back = new ImagePanel(0,0,w,h,"back.png");
+        back.setVisible(true);
+        this.add(back);
+
+        Shape = new ImagePanel(0,0,w,h,pic) ;
+        Shape.setVisible(false);
+        Shape.setBounds(x,y,w,h);
+        this.add(Shape,0);
+
+        setValue(s);
+        addMouseListener(this);
+    }
+    //Returns Value
     public int getValue(){
         return  value;
     }
-
-    /**
-     * Return a boolean that states whether card is flipped
-     *
-     */
+    //Return a boolean that states whether card is flipped
     public boolean isFlipped(){
         return flipped;
     }
-
-    /**
-    *Flips card
-     */
-
+    //Flips card
     public void getFlipped(){
         this.flipped = !this.flipped;
     }
-
-    /**
-    * Allows change of Value
-     */
+    //Allows change of Value
     public int setValue(int s){
         value = s;
         return value;
     }
-
-    /**
-     * Returns the String in the JTextArea
-     */
+    //Returns the String in the ImagePanel
     public String getQuestion(){
         return question;
     }
-
-    /**
-     * allows for the changed value of match
-     */
+    //allows for the changed value of match
     public void setMatched(boolean b)
     {
         matched=b;
     }
-
-    /**
-     * returns matched
-     */
+    //returns matched
     public boolean getMatched()
     {
         return matched;
     }
-
-    /**
-     * Allows the text in the JTextArea to be changed
-     */
+    //Allows the text in the ImagePanel to be changed
     public void setQuestion(String q){
         question = q;
     }
-
-    /**
-     * Flips all cards that are flipped up and not matched back over
-     *
-     */
+    //
     public void reset(ArrayList <Card> whatever) {
         for (Card c : whatever) {
             if ((c.flipped)&&(!matched)) {
                 back.setVisible(true);
-                Inquiry.setVisible(false);
+                Shape.setVisible(false);
                 c.flipped = false;
             }
         }
     }
-
-    /**
-     *Flips all cards flipped back over, resets all cards to not flipped
-     *
-     */
     public void resetAll(ArrayList<Card> whatever){
         for(int i = 0; i<whatever.size(); i++){
-            Inquiry.setVisible(false);
+            Shape.setVisible(false);
             back.setVisible(true);
         }
-        /**arbitrary array list name[i].flipped = false;
-         *
-         */
+        //arbitrary array list name[i].flipped = false;
     }
-
-    /**
-     * What happens when you click the JComponent/flips the card when clicked
-     */
+    //What happens when you click the JComponent
     @Override
     public void mouseClicked(MouseEvent e) {
         if (this.back.isVisible()) {
             back.setVisible(false);
-            Inquiry.setVisible(true);
+            Shape.setVisible(true);
             this.getFlipped();
         }
     }
@@ -151,4 +125,3 @@ public class Card extends JComponent implements MouseListener{
 
     }
 }
-
